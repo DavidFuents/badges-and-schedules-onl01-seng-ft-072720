65f844ae0567d08483122231bb51a3bb43c22273
David Fuentes <davidf0827@gmail.com>
2020-07-30 03:48:59 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,15 +12,12 @@ def batch_badge_creator(array)
 end
 
 def assign_rooms(array)
-  num_of_rooms = 7
+  num_of_rooms = 1
   list_of_rooms = []
   
   array.each do |speaker|
     list_of_rooms << "Hello, #{speaker}! You'll be assigned to room #{num_of_rooms}!"
-    num_of_rooms -= 1
-    if list_of_rooms == 0 
-      break
-    end 
+    num_of_rooms += 1
   end
   return list_of_rooms
 end
