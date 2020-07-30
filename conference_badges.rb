34aecca9ad8e23bc324efaa322ed040ba8d6e120
David Fuentes <davidf0827@gmail.com>
2020-07-30 04:01:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -28,7 +28,7 @@ def printer
   
   array.each do |attendee|
     batch_badge_creator[attendee] << personal_message
-    assign_rooms[attendee]
+    assign_rooms[attendee] << personal_message
     binding.pry
     puts personal_message[counter]
   end
